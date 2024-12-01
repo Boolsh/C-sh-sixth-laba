7d4864cf0ef627fa561c3a01e839cfe3fec80df6
Boolsh <pekshin05@mail.ru>
2024-12-01 23:08:26 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace lab6._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            textBoxSize = new TextBox();
+            btnCreate = new Button();
+            btnLoadMatrix = new Button();
+            btnTask = new Button();
+            btnSaveOriginal = new Button();
+            btnSaveResult = new Button();
+            dataGridViewInput = new DataGridView();
+            dataGridViewResult = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewInput).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewResult).BeginInit();
+            SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(24, 24);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // textBoxSize
+            // 
+            textBoxSize.ForeColor = Color.Gray;
+            textBoxSize.Location = new Point(18, 13);
+            textBoxSize.Margin = new Padding(2, 2, 2, 2);
+            textBoxSize.Multiline = true;
+            textBoxSize.Name = "textBoxSize";
+            textBoxSize.Size = new Size(126, 39);
+            textBoxSize.TabIndex = 0;
+            textBoxSize.Text = "Размер матрицы";
+            textBoxSize.Enter += textBoxSize_Enter;
+            textBoxSize.Leave += textBoxSize_Leave;
+            // 
+            // btnCreate
+            // 
+            btnCreate.Location = new Point(175, 13);
+            btnCreate.Margin = new Padding(2, 2, 2, 2);
+            btnCreate.Name = "btnCreate";
+            btnCreate.Size = new Size(81, 37);
+            btnCreate.TabIndex = 1;
+            btnCreate.Text = "Создать";
+            btnCreate.UseVisualStyleBackColor = true;
+            btnCreate.Click += btnCreate_Click;
+            // 
+            // btnLoadMatrix
+            // 
+            btnLoadMatrix.Location = new Point(286, 13);
+            btnLoadMatrix.Margin = new Padding(2, 2, 2, 2);
+            btnLoadMatrix.Name = "btnLoadMatrix";
+            btnLoadMatrix.Size = new Size(154, 37);
+            btnLoadMatrix.TabIndex = 2;
+            btnLoadMatrix.Text = "Загрузить матрицу";
+            btnLoadMatrix.UseVisualStyleBackColor = true;
+            btnLoadMatrix.Click += btnLoadMatrix_Click;
+            // 
+            // btnTask
+            // 
+            btnTask.Location = new Point(464, 13);
+            btnTask.Margin = new Padding(2, 2, 2, 2);
+            btnTask.Name = "btnTask";
+            btnTask.Size = new Size(195, 37);
+            btnTask.TabIndex = 3;
+            btnTask.Text = "Удалить столбцы, с заданным значением";
+            btnTask.UseVisualStyleBackColor = true;
+            btnTask.Click += btnTask_Click;
+            // 
+            // btnSaveOriginal
+            // 
+            btnSaveOriginal.Location = new Point(64, 79);
+            btnSaveOriginal.Margin = new Padding(2, 2, 2, 2);
+            btnSaveOriginal.Name = "btnSaveOriginal";
+            btnSaveOriginal.Size = new Size(141, 20);
+            btnSaveOriginal.TabIndex = 4;
+            btnSaveOriginal.Text = "Сохраниь исходную";
+            btnSaveOriginal.UseVisualStyleBackColor = true;
+            btnSaveOriginal.Click += btnSaveOriginal_Click;
+            // 
+            // btnSaveResult
+            // 
+            btnSaveResult.Location = new Point(508, 79);
+            btnSaveResult.Margin = new Padding(2, 2, 2, 2);
+            btnSaveResult.Name = "btnSaveResult";
+            btnSaveResult.Size = new Size(134, 20);
+            btnSaveResult.TabIndex = 5;
+            btnSaveResult.Text = "Сохранить";
+            btnSaveResult.UseVisualStyleBackColor = true;
+            btnSaveResult.Click += btnSaveResult_Click;
+            // 
+            // dataGridViewInput
+            // 
+            dataGridViewInput.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewInput.Location = new Point(18, 108);
+            dataGridViewInput.Margin = new Padding(2, 2, 2, 2);
+            dataGridViewInput.Name = "dataGridViewInput";
+            dataGridViewInput.RowHeadersWidth = 62;
+            dataGridViewInput.Size = new Size(377, 194);
+            dataGridViewInput.TabIndex = 6;
+            // 
+            // dataGridViewResult
+            // 
+            dataGridViewResult.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewResult.Location = new Point(464, 108);
+            dataGridViewResult.Margin = new Padding(2, 2, 2, 2);
+            dataGridViewResult.Name = "dataGridViewResult";
+            dataGridViewResult.RowHeadersWidth = 62;
+            dataGridViewResult.Size = new Size(381, 194);
+            dataGridViewResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(864, 326);
+            Controls.Add(dataGridViewResult);
+            Controls.Add(dataGridViewInput);
+            Controls.Add(btnSaveResult);
+            Controls.Add(btnSaveOriginal);
+            Controls.Add(btnTask);
+            Controls.Add(btnLoadMatrix);
+            Controls.Add(btnCreate);
+            Controls.Add(textBoxSize);
+            Margin = new Padding(2, 2, 2, 2);
+            Name = "Form1";
+            Text = "Matrix Manipulation";
+            ((System.ComponentModel.ISupportInitialize)dataGridViewInput).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewResult).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private ContextMenuStrip contextMenuStrip1;
+        private TextBox textBoxSize;
+        private Button btnCreate;
+        private Button btnLoadMatrix;
+        private Button btnTask;
+        private Button btnSaveOriginal;
+        private Button btnSaveResult;
+        private DataGridView dataGridViewInput;
+        private DataGridView dataGridViewResult;
+    }
+}
---
+++ MatrMake.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6._2
+{
+    class MatrMake
+    {
+        int n_str,     //количество строк 
+            n_col;     //количество столбцов 
+        int[,] matrix; // обрабатываемая матрица
+        public MatrMake(int n)
+        {
+            n_str = n;
+            n_col = n; // на входе матрица квадратная 
+            matrix = new int[n, n];
+        }
+        //матрица из DataGridView 
+        public void GridToMatrix(DataGridView dgv)
+        {
+            DataGridViewCell txtCell;
+            for (int i = 0; i < n_str; i++)
+            {
+                for (int j = 0; j < n_col; j++)
+                {
+                    txtCell = dgv.Rows[i].Cells[j];
+                    string s = txtCell.Value.ToString();
+                    if (s == "")
+                        matrix[i, j] = 0;
+                    else
+                        matrix[i, j] = Int32.Parse(s);
+                }
+            }
+        }
+        //вывод матрицы в DataGridView 
+        public void MatrixToGrid(DataGridView dgv)
+        {
+            //установка размеров 
+            int i;
+            DataTable matr = new DataTable("matr");
+            DataColumn[] cols = new DataColumn[n_col];
+            for (i = 0; i < n_col; i++)
+            {
+                cols[i] = new DataColumn(i.ToString());
+                matr.Columns.Add(cols[i]);
+            }
+            for (i = 0; i < n_str; i++)
+            {
+                DataRow newRow;
+                newRow = matr.NewRow();
+                matr.Rows.Add(newRow);
+            }
+            dgv.DataSource = matr;
+            for (i = 0; i < n_col; i++)
+                dgv.Columns[i].Width = 50;
+            //занесение значений
+            DataGridViewCell txtCell;
+            for (i = 0; i < n_str; i++)
+            {
+                for (int j = 0; j < n_col; j++)
+                {
+                    txtCell = dgv.Rows[i].Cells[j];
+                    txtCell.Value = matrix[i, j].ToString();
+                }
+            }
+        }
+        public void DelCol(int value) // Метод для удаления столбцов, содержащих заданное значение
+        {
+            List<int> columnsToRemove = new List<int>();
+
+            // Определяем, какие столбцы нужно удалить
+            for (int j = 0; j < n_col; j++)
+            {
+                for (int i = 0; i < n_str; i++)
+                {
+                    if (matrix[i, j] == value)
+                    {
+                        columnsToRemove.Add(j);
+                        break;
+                    }
+                }
+            }
+
+            // Удаляем столбцы с конца, чтобы не нарушать индексы
+            for (int i = columnsToRemove.Count - 1; i >= 0; i--)
+            {
+                int col = columnsToRemove[i]; // Получаем индекс столбца для удаления
+                for (int j = 0; j < n_str; j++)
+                {
+                    for (int k = col; k < n_col - 1; k++)
+                    {
+                        matrix[j, k] = matrix[j, k + 1];
+                    }
+                    matrix[j, n_col - 1] = 0; // Обнуляем последний столбец
+                }
+                n_col--; // Уменьшаем количество столбцов
+            }
+        }
+        public int GetCol()
+        {
+            return n_col;
+        }
+        public int GetStr()
+        {
+            return n_str;
+        }
+    }
+}
